adfc29d1c743e7ca324884a1b7cf33d45e228199
vatsalgada <contactvgada@gmail.com>
2021-07-12 17:07:51 +0530

    npm files and database connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,10 +12,43 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/todov2', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+  name:{
+    type: String
+  }
+};
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+const item2 = new Item({
+  name: "Hit the + button to add new items" 
+})
+const item3 = new Item({
+  name: "<-- Hit the checkbox to delete an item"
+})
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems , function(err){
+  if (err){
+    console.log(err);
+  } else{
+    console.log("Succesfully saved default items to DB");  
+  }
+}  )
+
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
 app.get("/", function(req, res) {
+
+  Item.find({}, function(err, foundItems){
+    console.log(foundItems);
+  })
 
 const day = date.getDate();
 
